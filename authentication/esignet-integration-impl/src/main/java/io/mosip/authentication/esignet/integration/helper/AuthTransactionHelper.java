93555ae54b2b74cd17f87049af5b0f91d3dd8a48
Shivam Rajesh Gupta <56411166+shivamg9@users.noreply.github.com>
2026-06-01 23:12:09 +0530

    Fix cache key for auth token retrieval
    Signed-off-by: Shivam Rajesh Gupta <56411166+shivamg9@users.noreply.github.com>

--- authentication/esignet-integration-impl/src/main/java/io/mosip/authentication/esignet/integration/helper/AuthTransactionHelper.java
+++ authentication/esignet-integration-impl/src/main/java/io/mosip/authentication/esignet/integration/helper/AuthTransactionHelper.java
@@ -47,7 +47,7 @@ public class AuthTransactionHelper {
     @Value("${mosip.esignet.authenticator.ida.app-id}")
     private String appId;
 	
-    @Cacheable(value = AUTH_TOKEN_CACHE, key = "#root.target.AUTH_TOKEN_CACHE_KEY")
+    @Cacheable(value = AUTH_TOKEN_CACHE, key = "'auth_token'")
     public String getAuthToken() throws Exception {
     	log.info("Started to get auth-token with appId : {} && clientId : {}",
                 appId, clientId);
