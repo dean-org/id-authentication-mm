a1ee144f4e9c8acdb6e12a2182a774d4c9111ee9
Dharanidharan-S-04 <dean12345.dd@gmail.com>
2026-06-10 23:10:18 +0530

    Update VciFacadeImpl.java
    Signed-off-by: Dharanidharan-S-04 <dean12345.dd@gmail.com>

--- authentication/authentication-service/src/main/java/io/mosip/authentication/service/kyc/facade/VciFacadeImpl.java
+++ authentication/authentication-service/src/main/java/io/mosip/authentication/service/kyc/facade/VciFacadeImpl.java
@@ -163,7 +163,7 @@ public class VciFacadeImpl implements VciFacade {
 
 			boolean isBioRequired = false;
 			if (filterAttributes.contains("faceRawImage")) {
-			    policyAllowedAttributes.add("faceRawImage");
+			    policyAllowedAttributes.add(CbeffDocType.FACE.getType().value().toLowerCase());
 			    isBioRequired = true;
 			} 
 			else if (filterAttributes.contains(CbeffDocType.FACE.getType().value().toLowerCase()) || 
